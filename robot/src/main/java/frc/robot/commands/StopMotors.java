15bd4591905a193ca3ec21aa47528b7f70851ff8
Jason Markham <jason@millsmart.com>
2022-02-17 14:56:31 -0800

    create some util commands

---
+++ robot/src/main/java/frc/robot/commands/StopMotors.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.RobotSubsystem;
+
+public class StopMotors extends RobotCommand {
+
+  public StopMotors(RobotSubsystem subsystem) {
+    super(subsystem);
+  }
+
+  @Override
+  public void initialize() {
+    for(var talon : subsystem.talons){
+      talon.neutralOutput();
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
